a92456fc6189372914fbcf1d1bac4e002abb5da9
gebretewodros73 <gebretewodros73@gmail.com>
2023-10-05 23:48:29 +0300

    floor calculation

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Module for floor calculation
+"""
+
+
+def floor(n: float) -> int:
+    """
+    Function to calculate floor of a float
+    Args:
+        n: input float number
+    Returns:
+        int: floor of the input number
+    """
+    return int(n)
