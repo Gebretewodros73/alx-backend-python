8d0d708c3630ea20d60dd85b695af06080b2e799
gebretewodros73 <gebretewodros73@gmail.com>
2023-10-05 23:55:10 +0300

    convert float to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Module for converting float to string
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Function to convert a float to its string representation
+    Args:
+        n: input float number
+    Returns:
+        str: string representation of the input number
+    """
+    return str(n)
