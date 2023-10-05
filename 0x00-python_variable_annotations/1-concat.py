05a32017d6ff4408ac21f8e0184d357c0d4e7e9f
gebretewodros73 <gebretewodros73@gmail.com>
2023-10-05 23:39:45 +0300

    string concatenate

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Module for concatenate of two string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenates two strings.
+
+    Args:
+        str1: The first string.
+        str2: The second string.
+
+    Returns:
+        The concatenated string.
+    """
+    return str1 + str2
