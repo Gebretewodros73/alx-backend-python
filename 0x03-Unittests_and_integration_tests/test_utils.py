4c9c3c57ba50c2def60437aa3904d83e646c2740
gebretewodros73 <gebretewodros73@gmail.com>
2023-10-28 08:07:01 +0300

    unit test for utils.get_json that mocks the HTTP calls using unittest.mock.patch.

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,26 +2,25 @@
 
 import unittest
 from parameterized import parameterized
-from unittest.mock import patch
-from utils import access_nested_map
+from unittest.mock import patch, Mock
+from utils import get_json
 
-class TestAccessNestedMap(unittest.TestCase):
+class TestGetJson(unittest.TestCase):
+    
     @parameterized.expand([
-        ({"a": 1}, ("a",), 1),
-        ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2),
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
     ])
-    def test_access_nested_map(self, nested_map, path, expected_result):
-        result = access_nested_map(nested_map, path)
-        self.assertEqual(result, expected_result)
+    @patch('utils.requests.get')
+    def test_get_json(self, test_url, test_payload, mock_get):
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+        mock_get.return_value = mock_response
 
-    @parameterized.expand([
-        ({}, ("a",), KeyError),
-        ({"a": 1}, ("a", "b"), KeyError),
-    ])
-    def test_access_nested_map_exception(self, nested_map, path, expected_exception):
-        with self.assertRaises(expected_exception):
-            access_nested_map(nested_map, path)
+        result = get_json(test_url)
+
+        mock_get.assert_called_once_with(test_url)
+        self.assertEqual(result, test_payload)
 
 if __name__ == '__main__':
     unittest.main()
