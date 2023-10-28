76a72e4897f0e34699618f51cc32c0bacc12695c
gebretewodros73 <gebretewodros73@gmail.com>
2023-10-28 08:19:11 +0300

    unit test to verify that memoization is working correctly using unittest.mock.patch.

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,7 +3,7 @@
 import unittest
 from parameterized import parameterized
 from unittest.mock import patch, Mock
-from utils import get_json
+from utils import get_json, memoize
 
 class TestGetJson(unittest.TestCase):
     
@@ -22,5 +22,25 @@ class TestGetJson(unittest.TestCase):
         mock_get.assert_called_once_with(test_url)
         self.assertEqual(result, test_payload)
 
+class TestMemoize(unittest.TestCase):
+
+    class TestClass:
+        def a_method(self):
+            return 42
+
+        @memoize
+        def a_property(self):
+            return self.a_method()
+
+    def test_memoize(self):
+        test_obj = self.TestClass()
+        with patch.object(self.TestClass, 'a_method', return_value=42) as mock_method:
+            result1 = test_obj.a_property
+            result2 = test_obj.a_property
+            mock_method.assert_called_once()
+            # Ensure a_method is called only once
+            self.assertEqual(result1, result2)
+
 if __name__ == '__main__':
     unittest.main()
+
