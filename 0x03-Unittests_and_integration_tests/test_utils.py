d5e6d87b1c369f48050fcfe4e7b0a479e4b9d56e
gebretewodros73 <gebretewodros73@gmail.com>
2023-10-30 02:44:59 +0300

    unit test to verify that memoization is working correctly using unittest.mock.patch.

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,6 +2,7 @@
 """
 Generic utilities for github org client.
 """
+
 import unittest
 from parameterized import parameterized
 from unittest.mock import patch, Mock
@@ -9,6 +10,9 @@ from utils import get_json, memoize
 
 
 class TestGetJson(unittest.TestCase):
+    """
+    Test class for get_json function.
+    """
 
     @parameterized.expand([
         ("http://example.com", {"payload": True}),
@@ -16,34 +20,66 @@ class TestGetJson(unittest.TestCase):
     ])
     @patch('utils.requests.get')
     def test_get_json(self, test_url, test_payload, mock_get):
+        """
+        Test get_json function.
+        """
+
+        # Create a mock response object
         mock_response = Mock()
         mock_response.json.return_value = test_payload
         mock_get.return_value = mock_response
 
+        # Call the function under test
         result = get_json(test_url)
 
+        # Assert that the mock get was called with the correct URL
         mock_get.assert_called_once_with(test_url)
+        # Assert that the result matches the expected payload
         self.assertEqual(result, test_payload)
 
 
 class TestMemoize(unittest.TestCase):
+    """
+    Test class for memoize decorator.
+    """
 
     class TestClass:
+        """
+        Test class with memoized method.
+        """
+
         def a_method(self):
+            """
+            Return 42.
+            """
             return 42
 
         @memoize
         def a_property(self):
+            """
+            Memoized property.
+            """
             return self.a_method()
 
     def test_memoize(self):
+        """
+        Test memoize decorator.
+        """
+
+        # Create an instance of the TestClass
         test_obj = self.TestClass()
+
+        # Mock the a_method with a return value of 42
         with patch.object(self.TestClass, 'a_method', return_value=42)\
                 as mock_method:
+            # Call the memoized property twice
             result1 = test_obj.a_property
             result2 = test_obj.a_property
+
+            # Assert that the mock method was called once
             mock_method.assert_called_once()
-            # Ensure a_method is called only once
+
+            # Ensure a_method is called only once and the results are equal
             self.assertEqual(result1, result2)
 
 
