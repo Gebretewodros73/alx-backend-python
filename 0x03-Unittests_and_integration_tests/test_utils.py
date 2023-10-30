d29b446b43600546bb91e2331edc8dc6abdda9fb
gebretewodros73 <gebretewodros73@gmail.com>
2023-10-30 20:05:55 +0300

    unit test to verify that memoization is working correctly using unittest.mock.patch.

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,37 +1,86 @@
 #!/usr/bin/env python3
-
 """
-Unit tests for client.GithubOrgClient class.
+Generic utilities for github org client.
 """
 
 import unittest
-from unittest.mock import patch
 from parameterized import parameterized
-from client import GithubOrgClient
+from unittest.mock import patch, Mock
+from utils import get_json, memoize
 
 
-class TestGithubOrgClient(unittest.TestCase):
+class TestGetJson(unittest.TestCase):
     """
-    Test cases for GithubOrgClient class.
+    Test class for get_json function.
     """
 
     @parameterized.expand([
-        ("google", {"payload": True}),
-        ("abc", {"payload": False}),
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
     ])
-    @patch('client.get_json')
-    def test_org(self, org, expected_payload, mock_get_json):
+    @patch('utils.requests.get')
+    def test_get_json(self, test_url, test_payload, mock_get):
         """
-        Test org method of GithubOrgClient class.
+        Test get_json function.
         """
-        mock_get_json.return_value = expected_payload
 
-        github_client = GithubOrgClient(org)
-        result = github_client.org
+        # Create a mock response object
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+        mock_get.return_value = mock_response
 
-        mock_get_json.assert_called_once_with(
-            f"https://api.github.com/orgs/{org}")
-        self.assertEqual(result, expected_payload)
+        # Call the function under test
+        result = get_json(test_url)
+
+        # Assert that the mock get was called with the correct URL
+        mock_get.assert_called_once_with(test_url)
+        # Assert that the result matches the expected payload
+        self.assertEqual(result, test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+    """
+    Test class for memoize decorator.
+    """
+
+    class TestClass:
+        """
+        Test class with memoized method.
+        """
+
+        def a_method(self):
+            """
+            Return 42.
+            """
+            return 42
+
+        @memoize
+        def a_property(self):
+            """
+            Memoized property.
+            """
+            return self.a_method()
+
+    def test_memoize(self):
+        """
+        Test memoize decorator.
+        """
+
+        # Create an instance of the TestClass
+        test_obj = self.TestClass()
+
+        # Mock the a_method with a return value of 42
+        with patch.object(self.TestClass, 'a_method', return_value=42)\
+                as mock_method:
+            # Call the memoized property twice
+            result1 = test_obj.a_property
+            result2 = test_obj.a_property
+
+            # Assert that the mock method was called once
+            mock_method.assert_called_once()
+
+            # Ensure a_method is called only once and the results are equal
+            self.assertEqual(result1, result2)
 
 
 if __name__ == '__main__':
