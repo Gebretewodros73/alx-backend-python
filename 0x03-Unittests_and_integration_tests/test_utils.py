e3f01e37adcdb6b51dc26cccc17de989f914f7ac
gebretewodros73 <gebretewodros73@gmail.com>
2023-10-30 13:33:02 +0300

    unit test for GithubOrgClient.org that uses @patch as a decorator to mock external HTTP calls.

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,73 +1,37 @@
 #!/usr/bin/env python3
 
 """
-Unit tests for utils module.
+Unit tests for client.GithubOrgClient class.
 """
 
 import unittest
-from unittest.mock import patch, Mock
+from unittest.mock import patch
 from parameterized import parameterized
-from utils import get_json, memoize
+from client import GithubOrgClient
 
 
-class TestGetJson(unittest.TestCase):
+class TestGithubOrgClient(unittest.TestCase):
     """
-    Test cases for get_json function.
+    Test cases for GithubOrgClient class.
     """
 
     @parameterized.expand([
-        ("http://example.com", {"payload": True}),
-        ("http://holberton.io", {"payload": False}),
+        ("google", {"payload": True}),
+        ("abc", {"payload": False}),
     ])
-    @patch('utils.requests.get')
-    def test_get_json(self, test_url, test_payload, mock_get):
+    @patch('client.get_json')
+    def test_org(self, org, expected_payload, mock_get_json):
         """
-        Test get_json function.
+        Test org method of GithubOrgClient class.
         """
-        mock_response = Mock()
-        mock_response.json.return_value = test_payload
-        mock_get.return_value = mock_response
+        mock_get_json.return_value = expected_payload
 
-        result = get_json(test_url)
+        github_client = GithubOrgClient(org)
+        result = github_client.org
 
-        mock_get.assert_called_once_with(test_url)
-        self.assertEqual(result, test_payload)
-
-
-class TestMemoize(unittest.TestCase):
-    """
-    Test cases for memoize decorator.
-    """
-
-    class TestClass:
-        """
-        Test class for memoize decorator.
-        """
-
-        def a_method(self):
-            """
-            Test method for memoize decorator.
-            """
-            return 42
-
-        @memoize
-        def a_property(self):
-            """
-            Test property for memoize decorator.
-            """
-            return self.a_method()
-
-    def test_memoize(self):
-        """
-        Test memoize decorator.
-        """
-        test_obj = self.TestClass()
-        with patch.object(self.TestClass, 'a_method', return_value=42)\
-                as mock_method:
-            result1 = test_obj.a_property
-            result2 = test_obj.a_property
-            mock_method.assert_called_once()
-            self.assertEqual(result1, result2)
+        mock_get_json.assert_called_once_with(
+            f"https://api.github.com/orgs/{org}")
+        self.assertEqual(result, expected_payload)
 
 
 if __name__ == '__main__':
